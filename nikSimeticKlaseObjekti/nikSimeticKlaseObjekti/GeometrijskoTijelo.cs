93214a1f652e1b261e58ed3ca9d59e442f405881
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-04-07 12:35:48 +0200

    Add files via upload

---
+++ nikSimeticKlaseObjekti/nikSimeticKlaseObjekti/GeometrijskoTijelo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticKlaseObjekti
+{
+    class GeometrijskoTijelo
+    {
+        double volumen;
+        double oplosje;
+        static int brojacTijela;
+
+        public double Volumen { get => volumen; set => volumen = value; }
+        public double Oplosje { get => oplosje; set => oplosje = value; }
+        public static int BrojacTijela { get => brojacTijela; set => brojacTijela = value; }
+
+        public GeometrijskoTijelo()
+        {
+            brojacTijela++;
+        }
+    }
+}
---
+++ nikSimeticKlaseObjekti/nikSimeticKlaseObjekti/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticKlaseObjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x = new Kugla();
+            Kvadar y = new Kvadar();
+            Kocka z = new Kocka();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Console.ReadKey();
+
+        }
+    }
+}
